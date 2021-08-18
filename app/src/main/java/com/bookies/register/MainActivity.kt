b2299a1878dc4ec168e0e9a439dc57502381e30f
Dipanshu Singh <dipanshuhappy@gmail.com>
2021-08-18 15:06:01 +0100

    test commit

--- app/src/main/java/com/bookies/register/MainActivity.kt
+++ app/src/main/java/com/bookies/register/MainActivity.kt
@@ -6,7 +6,6 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
