25f9410f5732e7142d4b41713038f09269adf8c4
Dipanshu Singh <dipanshuhappy@gmail.com>
2021-08-20 21:09:30 +0100

    splash screen made

--- app/src/main/java/com/bookies/register/SplashActivity.kt
+++ app/src/main/java/com/bookies/register/SplashActivity.kt
@@ -2,12 +2,11 @@ package com.bookies.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
